8f6a4abf22a4c5a4799672f8f004ccb826fdc79d
Diana397248 <du397248@gmail.com>
2020-08-21 21:27:50 +0400

    init

---
+++ my-projects/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const  Header= () =>{
+    return <header className="header">
+        <img src="https://gpoltava.com/assets/profiles_photo/7c2c753aa107f68e9f8532e69db3915a.jpg"/>
+    </header>;
+}
+
+export default Header;
---
+++ my-projects/src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const  Profile= () =>{
+    return <div className="content">
+        <div>
+            <img src="https://bm.img.com.ua/img/prikol/images/large/3/9/315193.jpg"/>
+        </div>
+        <div>
+            ava + deckrip
+        </div>
+        <div>
+            My post
+            <div>
+                New post
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
